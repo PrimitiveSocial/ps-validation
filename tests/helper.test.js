bb9ee208ab2802b1b03b656ab779ad1b3381c08c
Elie Andraos <elie@andraos.org>
2019-12-30 14:25:17 +0200

    :white_check_mark: Added starts_with helper tests

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,4 +1,4 @@
-import {isObject, hasArg} from "./../src/utils/Helper";
+import {isObject, hasArg, Str} from "./../src/utils/Helper";
 
 let validStr = 'required_if:hasEvent';
 test('it returns true with a valid syntax', () => {
@@ -18,4 +18,14 @@ test('it returns true with a valid object', () => {
 let invalidObj = "foo:bar";
 test('it returns false with an invalid object', () => {
   expect(isObject(invalidObj)).toBe(false);
+});
+
+let validStartsWith = "validation";
+test('Str starts_with is correct', () => {
+  expect(Str(validStartsWith).starts_with('va')).toBe(true);
+});
+
+let invalidStartsWith = "validation";
+test('Str starts_with is incorrect', () => {
+  expect(Str(invalidStartsWith).starts_with('inva')).toBe(false);
 });
